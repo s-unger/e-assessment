75b084d585eddea25139957eca20fa56ad845039
s <github_com@planetcat.de>
2021-06-02 15:08:14 +0200

    Added index site

---
+++ index.php
@@ -0,0 +1,11 @@
+<DOCTYPE html>
+<html>
+<head>
+<title>Mathe-Tiger</title>
+</head>
+<body>
+<h1>Werde zum Mathetiger!</h1>
+<p>Werde mit uns zum Mathetiger. Das hier ist die Startseite mit statischem und...</p>
+<?php echo "...dynamischem Content"; ?>
+</body>
+</html>
