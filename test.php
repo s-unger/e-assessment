2ac78fb4b25db5a137c62004cb94c6558ba59e0f
s <github_com@planetcat.de>
2021-06-09 13:17:54 +0200

    grundstruktur

---
+++ test.php
@@ -0,0 +1,10 @@
+<DOCTYPE html>
+<html>
+<head>
+<title>Test</title>
+</head>
+<body>
+<h1>Mache den Test</h1>
+<p>Finde hier heraus, wie gut du in Mathe bist. Immer wieder im Schuljahr werden Tests hier benotet.</p>
+</body>
+</html>
