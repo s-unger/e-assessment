88f574884a5e31d209ba41f6e3cb3be023446110
Stefanie <S.Pham@campus.lmu.de>
2021-06-13 11:47:17 +0200

    attempt to fix session start warning

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php session_start()?>
 <DOCTYPE html>
 <html>
 <head>
@@ -8,7 +9,6 @@
 <p>
     Frage 1: Numerical <br>
     <?php
-    session_start();
     include('questionGenerator.php');
     // call generate_numerical_1() to generate the first numerical question.
     // !Please call each generate function only once per Session (i.e. don't ask the same question twice per test).
