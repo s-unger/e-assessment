f7d041c264b32fc9bac7747ad2aa8217cf923653
Stefanie <S.Pham@campus.lmu.de>
2021-06-14 17:38:50 +0200

    adapt to main code structure

--- index.php
+++ index.php
@@ -1,61 +1,91 @@
-<?php session_start(); ?>
-<DOCTYPE html>
-<html>
+<?php session_start();
+include('questionGenerator.php'); ?>
+
+<html lang="en">
 <head>
-<title>Mathe-Tiger</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php
+    /*$q1 = $_POST['ans1']; TODO test evaluation
+    $q2 = $_POST['ans2'];
+    $q3 = $_POST['ans3'][0];
+    $correct = 0;
+
+    if ($q1 == "" || $q2 == "" || $q3 == "") {
+        echo '<h2>Please answer all questions!</h2>';
+    } else {
+        if ($q1 == $_SESSION['solution_truefalse_1']) {
+            $correct++;
+        } else {
+            echo '<p>Wrong answer!</p>';
+        }
+
+        if ($q2 == $_SESSION['solution_multiplechoice_1']) {
+            $correct++;
+        } else {
+            echo '<p>Wrong answer!</p>';
+        }
+
+        if ($q3 == $_SESSION['solution_numerical_1']) {
+            $correct++;
+        } else {
+            echo '<p>Wrong answer!</p>';
+        }
+        echo $correct;
+    }*/
+    ?>
 </head>
 <body>
-<h1>Werde zum Mathetiger!</h1>
-<p>
-    Frage 1: Numerical <br>
-    <?php
-    include('questionGenerator.php');
-    // call generate_numerical_1() to generate the first numerical question.
-    // !Please call each generate function only once per Session (i.e. don't ask the same question twice per test).
-    generate_numerical_1();
-    echo $_SESSION['question_numerical_1'] . "<br>";
+<form action="index.php" method="post">
+    <div class="q-group">
+        <p class="question"> Aufgabe 1</p>
+        <?php
+        generate_truefalse_1();
+        echo $_SESSION['question_truefalse_1'] . "<br>";
+        ?>
+        <input type="radio" name="ans1" value="true"><label>Richtig</label>
+        <input type="radio" name="ans1" value="false"><label>Falsch</label>
+    </div>
 
-    // call $_SESSION['solution_numerical_1'] to get the correct solution.
-    echo "Solution 1 from session: " . $_SESSION['solution_numerical_1'] . "<br>"; ?>
-    <br>
+    <div class="q-group">
+        <p class="question"> Aufgabe 2</p>
+        <?php
+        generate_multiplechoice_1();
+        echo $_SESSION['question_multiplechoice_1'] . "<br>";
+        $opt1 = $_SESSION['options_multiplechoice_1'][0];
+        $opt2 = $_SESSION['options_multiplechoice_1'][1];
+        $opt3 = $_SESSION['options_multiplechoice_1'][2];
+        $opt4 = $_SESSION['options_multiplechoice_1'][3];
+        ?>
+        <input type="radio" name="ans2" value="<?= $opt1 ?>"><label><?= $opt1 ?></label>
+        <input type="radio" name="ans2" value="<?= $opt2 ?>"><label><?= $opt2 ?></label>
+        <input type="radio" name="ans2" value="<?= $opt3 ?>"><label><?= $opt3 ?></label>
+        <input type="radio" name="ans2" value="<?= $opt4 ?>"><label><?= $opt4 ?></label>
+    </div>
 
-    Frage 2: Multiple Choice<br>
-    <?php
-    // call generate_multiplechoice_1() to generate the first multiple choice question.
-    generate_multiplechoice_1();
-    echo $_SESSION['question_multiplechoice_1'] . "<br>";
-    echo "option 1: " . $_SESSION['options_multiplechoice_1'][0] . "<br>";
-    echo "option 2: " . $_SESSION['options_multiplechoice_1'][1] . "<br>";
-    echo "option 3: " . $_SESSION['options_multiplechoice_1'][2] . "<br>";
-    echo "option 4: " . $_SESSION['options_multiplechoice_1'][3] . "<br>";
+    <div class="q-group">
+        <p class="question"> Aufgabe 3</p>
+        <?php
+        generate_numerical_1();
+        echo $_SESSION['question_numerical_1'] . "<br>";
+        ?>
+        <input type="number" name="ans3">
+    </div>
 
-    // call $_SESSION['solution_multiplechoice_1'] to get the correct solution.
-    echo "Solution 2 from session: " . $_SESSION['solution_multiplechoice_1'] . "<br>"; ?>
-    <br>
+    <div class="q-group">
+        <p class="question"> Aufgabe 4</p>
+        <?php
+        $question_4 = generate_matching_1();
+        echo $_SESSION['question_matching_1'] . "<br>";
+        echo "Options column 1 (fixed order): <br>" . $_SESSION['options_matching_1'][0][0] . "<br>" . $_SESSION['options_matching_1'][0][1] . "<br>" . $_SESSION['options_matching_1'][0][2] . "<br>";
+        echo "Options column 2 (user needs to rearrange these to match column 1): <br>" . $_SESSION['options_matching_1'][1][0] . "<br>" . $_SESSION['options_matching_1'][1][1] . "<br>" . $_SESSION['options_matching_1'][1][2] . "<br>";
+        echo "Solution 4 (column 2 in the correct order): <br>" . $_SESSION['solution_matching_1'][0] . "<br>" . $_SESSION['solution_matching_1'][1] . "<br>" . $_SESSION['solution_matching_1'][2] . "<br>"; ?>
+        <br>
+    </div>
 
-
-    Frage 3: True or False <br>
-    <?php
-    generate_truefalse_1();
-    echo $_SESSION['question_truefalse_1'] . "<br>";
-    echo "Solution: " . json_encode($_SESSION['solution_truefalse_1']) . "<br>";?>
-    <br>
-
-    Frage 4: Matching (Mock-Version, not randomized yet)<br>
-    <?php
-    $question_4 = generate_matching_1();
-    echo $_SESSION['question_matching_1'] . "<br>";
-    echo "Options column 1 (fixed order): <br>" . $_SESSION['options_matching_1'][0][0] . "<br>" . $_SESSION['options_matching_1'][0][1] . "<br>" . $_SESSION['options_matching_1'][0][2] . "<br>";
-    echo "Options column 2 (user needs to rearrange these to match column 1): <br>" . $_SESSION['options_matching_1'][1][0] . "<br>". $_SESSION['options_matching_1'][1][1] . "<br>". $_SESSION['options_matching_1'][1][2] . "<br>";
-
-    echo "Solution 4 (column 2 in the correct order): <br>" . $_SESSION['solution_matching_1'][0] . "<br>". $_SESSION['solution_matching_1'][1] . "<br>". $_SESSION['solution_matching_1'][2] . "<br>"; ?>
-    <br>
-
-    <?php
-    generate_matching_2();
-    ?>
-
-
-</p>
+    <input type="submit">
+</form>
 </body>
 </html>
