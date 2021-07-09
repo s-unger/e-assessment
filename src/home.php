bdff68d84994622f0f72d0453d17e4df199531f6
Stefanie <S.Pham@campus.lmu.de>
2021-07-09 11:36:41 +0200

    Test not submittable again after evaluation. Loccal 'session already started' warning fixed. Setup testing for database

--- src/home.php
+++ src/home.php
@@ -1,5 +1,4 @@
-<?php
-session_start();
+<?php if(!isset($_SESSION)) session_start();
 //redirect to basepage if not logged in
 if (!isset($_SESSION['userid'])) {
     header("Location: ../index.php");
@@ -9,20 +8,27 @@ include "../index.php"; ?>
     <?php include '../style.css'; ?>
 </style>
 <?php
+
+
+if ($_POST) {
+    if (isset($_POST['newExam'])) {
+        newExam();
+    }
+}
 if ($_POST) {
     if (isset($_POST['newTest'])) {
         newTest();
     }
 }
 
-function newTest()
-{
-    $_SESSION['newQuestions'] = true;
-}
-
 ?>
 <div class="content">
     <form action="test.php" method="post">
-        <input class="btn" type="submit" name="newTest" value="Mach den Test"/>
+        <input class="btn" type="submit" name="newTest" value="Übungsmodus"/>
     </form>
+</div>
+<div class="content">
+<form action="test.php" method="post">
+    <input class="btn" type="submit" name="newExam" value="Prüfungsmodus"/>
+</form>
 </div>
