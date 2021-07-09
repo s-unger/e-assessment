29924101b1c8291108bd6a2a55a8c5f13a9c12b6
Stefanie <S.Pham@campus.lmu.de>
2021-07-09 18:37:42 +0200

    Test not submittable in exam mode, exam mode evaluation includes grade, exam mode database set up to be tested

--- src/home.php
+++ src/home.php
@@ -7,28 +7,12 @@ include "../index.php"; ?>
 <style>
     <?php include '../style.css'; ?>
 </style>
-<?php
 
-
-if ($_POST) {
-    if (isset($_POST['newExam'])) {
-        newExam();
-    }
-}
-if ($_POST) {
-    if (isset($_POST['newTest'])) {
-        newTest();
-    }
-}
-
-?>
 <div class="content">
     <form action="test.php" method="post">
         <input class="btn" type="submit" name="newTest" value="Übungsmodus"/>
     </form>
-</div>
-<div class="content">
-<form action="test.php" method="post">
-    <input class="btn" type="submit" name="newExam" value="Prüfungsmodus"/>
-</form>
+    <form action="test.php" method="post">
+        <input class="btn" type="submit" name="newExam" value="Prüfungsmodus"/>
+    </form>
 </div>
