f58bb0433c12de8cc8046c6a12dc7cd7b5d96a7c
s <github_com@planetcat.de>
2021-07-12 02:08:16 +0200

    Added Virtual Reality Software and Code

---
+++ VRBrowser/MatheTiger/Intermediate/Source/MatheTiger.Build.cs
@@ -0,0 +1,12 @@
+using UnrealBuildTool;
+
+public class MatheTiger : ModuleRules
+{
+	public MatheTiger(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PrivateDependencyModuleNames.Add("Core");
+		PrivateDependencyModuleNames.Add("Core");
+	}
+}
