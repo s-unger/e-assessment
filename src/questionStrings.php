475d82201813616a7aa835a8a2b5c7cd89cc87f5
Sophia Münch <muench@cip.ifi.lmu.de>
2021-07-13 17:44:20 +0200

    small update after code suggestion and merge

--- src/questionStrings.php
+++ src/questionStrings.php
@@ -1,5 +1,5 @@
 <?php
-$actors = [["Der Mathetiger", "er"], ["Das Mathezebra", "es"], ["Die Mathekatze", "sie"]];
+$actors = [["Der Mathematiger", "er"], ["Das Mathezebra", "es"], ["Die Mathekatze", "sie"]];
 $actors2 = ["Das Musiknilpferd", "Der Deutschaffe", "Die Kunstmaus"];
 $questionTerms7 =  ["Multiplikation", "Addition", "Division", "Division", "Subtraktion", "Subtraktion"];
 $configTerms7 =  ["eine Zahl", "eine Zahl", "die erste Zahl", "die zweite Zahl", "die erste Zahl", "die erste Zahl"];
