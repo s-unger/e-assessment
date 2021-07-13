d46933b4dcb71c4911e7d41f927dc47e230563e9
Stefanie <S.Pham@campus.lmu.de>
2021-07-13 22:16:24 +0200

    question 7 terms fixed

--- src/questionStrings.php
+++ src/questionStrings.php
@@ -2,5 +2,5 @@
 $actors = [["Der Mathematiger", "er"], ["Das Mathezebra", "es"], ["Die Mathekatze", "sie"]];
 $actors2 = ["Das Musiknilpferd", "Der Deutschaffe", "Die Kunstmaus"];
 $questionTerms7 =  ["Multiplikation", "Addition", "Division", "Division", "Subtraktion", "Subtraktion"];
-$configTerms7 =  ["eine Zahl", "eine Zahl", "die erste Zahl", "die zweite Zahl", "die erste Zahl", "die erste Zahl"];
-$solutionTerms7 =  ["Faktor", "Summand", "Dividend", "Divisor", "Subtrahend", "Minuend"];
+$configTerms7 =  ["eine Zahl", "eine Zahl", "die erste Zahl", "die zweite Zahl", "die erste Zahl", "die zweite Zahl"];
+$solutionTerms7 =  ["Faktor", "Summand", "Dividend", "Divisor", "Minuend", "Subtrahend"];
